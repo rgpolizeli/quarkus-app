c1ec7b1f3b8059a3c8a676931b431c1e874f57d1
Ricardo Gelio Polizeli <ricardogp1993@gmail.com>
2021-06-13 09:46:28 -0300

    database initial inserts and configuration

---
+++ src/main/resources/import.sql
@@ -0,0 +1,16 @@
+INSERT INTO Exame(id,descricao,preco) VALUES (440293,'Endoscopia‎','150.2');
+INSERT INTO Exame(id,descricao,preco) VALUES (4029389,'Hemograma','55');
+INSERT INTO Exame(id,descricao,preco) VALUES (4920299,'Eletrocardiograma','221.85');
+
+INSERT INTO Medico(id,nome,especialidade) VALUES (3049583,'Carlos Ferreira Junior','Cardiologia');
+INSERT INTO Medico(id,nome,especialidade) VALUES (320192,'Joana Costa','Gastroenterologia');
+INSERT INTO Medico(id,nome,especialidade) VALUES (39403948,'Ana Santos e Morais','Clínica Geral');
+
+INSERT INTO Paciente(id,nome,dataNascimento,genero,endereco) VALUES (23049,'Paulo José','1993-05-01','Masculino', 'Rua das Palmeiras, 251 - Catanduva/SP');
+INSERT INTO Paciente(id,nome,dataNascimento,genero,endereco) VALUES (201938,'Marcos de Souza','1980-12-22','Masculino', 'Rua das Bandeiras, 3121 - Votuporanga/SP');
+INSERT INTO Paciente(id,nome,dataNascimento,genero,endereco) VALUES (209384,'Alberto Cavalquente Neto', '1952-09-10', 'Masculino',  'Rua Henrique da Costa, 31 - São Paulo/SP');
+INSERT INTO Paciente(id,nome,dataNascimento,genero,endereco) VALUES (211029,'Fabiana Lima Gonçales', '1975-03-11', 'Feminino',  'Rua Amazonas, 514 - Jales/SP');
+
+INSERT INTO PostoColeta(id,descricao,endereco) VALUES (12345,'Posto Norte','Rua Parana, 2562 - Jales/SP');
+INSERT INTO PostoColeta(id,descricao,endereco) VALUES (109837,'Posto Sul','Avenida Alcântara, 261 - Catanduva/SP');
+INSERT INTO PostoColeta(id,descricao,endereco) VALUES (182736,'Posto Noroeste','Rua Marcos Sato, 20 - São Paulo/SP');
